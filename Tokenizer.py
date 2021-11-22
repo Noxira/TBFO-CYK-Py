badfb76f2b12b53ecade44110e912fc88edcedce
Noxira <42240815+Noxira@users.noreply.github.com>
2021-11-22 08:08:55 +0700

    tokenizer v1.0

---
+++ Tokenizer.py
@@ -0,0 +1,147 @@
+import re
+import sys
+import os
+import shutil
+from typing import NamedTuple
+
+# # copies file to Tokenized
+# sys_path = sys.path[0]
+# destination_folder = "./Tokenized/"
+# destination_dir = os.path.join(sys_path, destination_folder)
+# shutil.copy("test.txt", destination_dir + "test.txt")
+
+class Token(NamedTuple):
+    type: str
+    value: str
+    line: int
+    column: int
+
+def tokenize(code):
+    token_specs = [
+        # VAR TYPES
+        ('TYPE_FLOAT', r'\d+\.\d+'),
+        ('TYPE_INT', r'\d+'),
+        ('TYPE_STR', r'\"[^"]*\"'),
+        ('TYPE_STR2', r"\'[^']*\'"),
+        ('BOOL_TRUE', r"True"),
+        ('BOOL_FALSE', r"False"),
+        ('TYPE_NONE', r"None"),
+        # ASSIGNMENT OP
+        ('ASSOP_PLUS', r'\+='),
+        ('ASSOP_MIN', r'\-='),
+        ('ASSOP_MULT', r'\*='),
+        ('ASSOP_DIV', r'\/='),
+        ('ASSOP_MOD', r'%='),
+        ('ASSOP_FLOOR', r'\/\/='),
+        ('ASSOP_EXP', r'\*\*='),
+        # ARITH OP
+        ('OP_FLOOR', r'\/\/'),
+        ('OP_EXP', r'\*\*'),
+        ('OP_PLUS', r'\+'),
+        ('OP_MIN', r'\-'),
+        ('OP_MULT', r'\*'),
+        ('OP_DIV', r'\/'),
+        ('OP_MOD', r'%'),
+        # COMMA DOT
+        ('DOT', r'\.'),
+        ('COMMA', r','),
+
+        # COMPARATOR
+        ('COMP_EQ', r'=='),
+        ('COMP_NEQ', r'!='),
+        ('COMP_GREATER_EQ', r'>='),
+        ('COMP_LESS_EQ', r'<='),
+        ('COMP_GREATER', r'>'),
+        ('COMP_LESS', r'<'),
+
+        # LOGICAL OP
+        ('LOGIC_NOT', r'!'),
+        ('LOGIC_AND', r'&'),
+        ('LOGIC_OR', r'\|'),
+        ('LOGIC_AND2', r'and\s+'),
+        ('LOGIC_NOT2', r'not\s+'),
+        ('LOGIC_OR2', r'or\s+'),
+
+        
+        # ASSOP
+        ('ASSOP', r'='),
+        
+        # BRACKETS AND FRIENDS
+        ('OPEN_PAREN', r'\('),
+        ('CLOSE_PAREN', r'\)'),
+        ('OPEN_BRACK', r'\['),
+        ('CLOSE_BRACK', r'\]'),
+        ('OPEN_CBRACK', r'{'),
+        ('CLOSE_CBRACK', r'}'),
+
+        # KEYWORDS
+        ('FROM', r'from\s+'),
+        ('IMPORT', r'import\s+'),
+        ('AS', r'as\s+'),
+        ('IN', r'in\s+'),
+        ('IS', r'is\s+'),
+        ('CONT', r'continue'),
+        ('CLASS', r'class\s+'),
+        ('DEF', r'def\s+',),
+        ('PASS', r'pass'),
+        ('RETURN', r'return\W'),
+        ('IF', r'if\W'),
+        ('ELIF', r'elif\W'),
+        ('ELSE', r'else'),
+        ('FOR', r'for\s+'),
+        ('RANGE', r'range'),
+        ('WHILE', r'while\W'),
+        # (r'raise\s+'),
+        # (r'with\s+'),
+        ('COLON', r':'),
+
+        # IDENTIFIER
+        ('ID',       r'[A-Za-z][A-Za-z0-9\_]*'),
+
+        # END
+        ('NEWLINE',  r'\n'),           # Line endings
+        ('SKIP',     r'[ \t]+'),       # Skip over spaces and tabs
+        ('MISMATCH', r'.'),            # Any other character
+    ]
+
+    tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specs)
+    line_num = 1
+    line_start = 0
+    for mo in re.finditer(tok_regex, code):
+        kind = mo.lastgroup
+        value = mo.group()
+        column = mo.start() - line_start
+        if kind == 'NUMBER':
+            value = float(value) if '.' in value else int(value)
+        elif kind == 'NEWLINE':
+            line_start = mo.end()
+            line_num += 1
+            continue
+        elif kind == 'SKIP':
+            continue
+        elif kind == 'MISMATCH':
+            raise RuntimeError(f'{value!r} unexpected on line {line_num}')
+        yield Token(kind, value, line_num, column)
+
+
+
+statements = """"""
+file = open('test.txt', 'r')
+read_content = file.read()
+statements = statements + read_content
+file.close()
+
+result = """"""
+line = int(1)
+for token in tokenize(statements):
+    if token.line != line:
+        line += 1
+        result += '\n'
+
+    result += token.type + ' '
+
+print(result)
+fileRes = open('tokenized.txt', 'a')
+fileRes.truncate(0) #removes whatevers inside
+fileRes.write(result)
+fileRes.close()
